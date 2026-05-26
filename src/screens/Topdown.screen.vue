73e95dbd60406f32beabeb0e6e4ea5ea65572060
Павел Бездорнов <alpinweb@yandex.ru>
2026-05-26 15:27:44 +0600

    Delete widgets

--- src/screens/Topdown.screen.vue
+++ src/screens/Topdown.screen.vue
@@ -3,15 +3,11 @@ import { onBeforeUnmount, onMounted, ref } from "vue";
 import Phaser from "phaser";
 import { TopdownScene } from "@/scenes/Topdown.scene";
 import Preloader from "@/ui-components/Preloader.component.vue";
-import HealthBar from "@/ui-components/HealthBar.component.vue";
-import UiAnchor from "@/ui-components/UiAnchor.component.vue";
 import { usePlayer } from "@/store/player.store";
-import { PLAYER_MAX_HEALTH } from "@/configs/gameplay.config";
 import { LEVEL_WIDTH, LEVEL_HEIGHT } from "@/configs/engine.config";
 import { router } from "@/router.js";
 import { EventBus } from "@/utils/utils.js";
 import * as EventNames from "@/configs/eventNames.config.js";
-import LanguageSwitcher from "@/ui-components/LanguageSwitcher.vue";
 
 const gameContainer = ref(null);
 const playerStore = usePlayer();
@@ -53,12 +49,6 @@ onBeforeUnmount(() => {
 <template>
   <div class="game-screen">
     <Preloader />
-    <UiAnchor anchor="top-right" :offset-x="10" :offset-y="10" target=".game-screen__game-wrapper">
-      <LanguageSwitcher />
-    </UiAnchor>
-    <UiAnchor anchor="top-left" :offset-x="30" :offset-y="30" target=".game-screen__game-wrapper">
-      <HealthBar :max-health="PLAYER_MAX_HEALTH" :current-health="playerStore.currentHealth" />
-    </UiAnchor>
     <div ref="gameContainer" class="game-screen__game-wrapper"></div>
   </div>
 </template>
